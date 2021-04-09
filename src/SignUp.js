c9f44c1864e50c0423bede3e88d3c2a9ac300b8f
Abdael Mora <azmoracards@gmail.com>
2021-04-09 06:41:24 -0700

    login page phase 1

---
+++ src/SignUp.js
@@ -0,0 +1,26 @@
+import React from "react";
+//import "./Login.css";
+function signUp(props) {
+  return (
+    <div id="SignUpTop">
+      <h1 id="welcome">welcome to app</h1>
+      <form id="SignUp-form">
+        <label>
+          <p id="sighnUpTittles">Username</p>
+          <input type="text" placeholder="Username" id="inputBox" />
+        </label>
+        <label>
+          <p id="signUpTittles">Password</p>
+          <input type="password" placeholder="Password" id="inputBox" />
+        </label>
+        <div>
+          <div id="space"></div>
+          <button type="submit" id="SignButton">
+            SignUp
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+export default signUp;
