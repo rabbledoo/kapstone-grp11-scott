d324bdf46b3f8d36bc6a1066798369aca6d08d28
Scott Tarnowsky <scott.tarnowsky@gmail.com>
2021-04-09 10:32:43 -0400

    imported abdael's login branch

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-
-import login from "./Login";
+import login from "./components/Login/Login";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
-import signUp from "./SignUp";
-
+import signUp from "./components/SignUp/SignUp";
 
 function App() {
-  function testGet() {
-    fetch("http:localhost:3000")
-      .then((res) => res.text())
-      .then((data) => console.log(data));
-  }
+  // function testGet() {
+  //   fetch("http:localhost:3000")
+  //     .then((res) => res.text())
+  //     .then((data) => console.log(data));
+  // }
 
   return (
-
     <div classname="App">
       <h1 id="tittle">Creative Kritique</h1>
       <Switch>
@@ -21,7 +18,6 @@ function App() {
         <Route exact path="/signup" component={signUp} />
       </Switch>
     </div>
-
   );
 }
 
