1a9e187a29a9c5db15c8fffd786c0f9db6a5e19a
Scott Tarnowsky <scott.tarnowsky@gmail.com>
2021-04-11 18:23:31 -0400

    removing the redux pattern in store

--- src/store/store.js
+++ src/store/store.js
@@ -1,29 +1,7 @@
 // imports here...
-
-// define the store's initial state
-let initialState = { user: { token: "" }, messages: [] };
-
-// set action types
-export const LOGIN = "LOGIN";
-export const LOGOUT = "LOGOUT";
+import create from "zustand";
+import { devtools } from "zustand/middleware";
 
 
 
-// define reducer function
-const reducer = (state, action) => {
-  switch (action.type) {
-    case LOGIN:
-      return { user: action.payload };
-    case LOGOUT:
-      return { user: {} };
-    // case GET_MESSAGES:
-    //   return { ...state, messages: action.payload };
-    default:
-      return state;
-  }
-};
-    
-
-// create useStore hook
-
-export const useStore = create(devtools(redux(reducer, initialState)), { name: "storage" });
+export default create(devtools(useStore));
