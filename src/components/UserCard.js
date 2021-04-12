df8246a0110f107eca225e028c695b0b98dbc3d1
Abdael Mora <azmoracards@gmail.com>
2021-04-12 12:09:39 -0700

    updated login/signup added user card

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -16,20 +16,6 @@ function Kard() {
         </li>
       </ul>
     </div>
-
-    // <div class="card1">
-    //   <img
-    //     src="https://upload.wikimedia.org/wikipedia/en/thumb/c/c5/Bob_the_builder.jpg/220px-Bob_the_builder.jpg"
-    //     class="user-image"
-    //     alt="..."
-    //   />
-    //   <div class="card-body">
-    //     <h3 class="username">Bob the Builder</h3>
-    //     <h4 class="friends">friends: 10</h4>
-
-    //     <h5 class="date">date joined: 2 months ago</h5>
-    //   </div>
-    // </div>
   );
 }
 export default Kard;
