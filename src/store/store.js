a520a22aadce7fb61dd6557d5f1ae85cc3408887
Scott Tarnowsky <scott.tarnowsky@gmail.com>
2021-04-15 09:08:45 -0400

    set up global store

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,36 @@
 // imports here...
-// // imports here...
-// import create from "zustand";
-// import { devtools } from "zustand/middleware";
+import create from "zustand";
+import { devtools } from "zustand/middleware";
+const initialState = { user: { token: "" } };
+export const useStore = create(
+  devtools((set) => ({
+    initialState,
+    setNewUser: (url, formData) => {
+      return fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name: formData.name,
+          displayName: formData.displayName,
+          password: formData.password,
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) =>
+          set( { 
+            user: { name: data.name, displayName: data.displayName, token: initialState.user.token },
+          })
+        );
+    },
+  }))
+);
 
-
-
-// export default create(devtools(useStore));
+// return fetch(baseURL + "users", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify(registerData),
+// })
+//   .then((res) => res.json())
+//   .then((data) => console.log(data))
