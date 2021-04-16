6359e4ac69d80f10a890cee06cca9a213e2a9fb7
Megan Dimond <medimond83@gmail.com>
2021-04-16 11:23:43 -0400

    HomePage Styling version 1.1

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,28 +1,46 @@
-import React from 'react';
+import React from "react";
 // import { useStore } from '../store/store';
-import Login from '../components/Login/Login'
-import SignUp from '../components/SignUp/SignUp';
-
-
-
+import { Container, Col, Row } from "react-bootstrap";
+import Login from "../components/Login/Login";
+import SignUp from "../components/SignUp/SignUp";
 
 function HomePage(props) {
-    // const user = useStore((state) => state.user);
+  // const user = useStore((state) => state.user);
 
-    return (
-      <>
-      
-        <h1> Kreative Kritique </h1>
-
-        <Login />
-        <SignUp />
-        
-      </>
-    );
+  return (
+    <div id="HomePage">
+      <Container id="Homepage">
+        <Row>
+          <Col>
+            <h1> Kreative Kritique </h1>
+            <p>
+              A place to share your original creative works and get critiques
+              from other creative people.
+            </p>
+            <p>
+              <strong>Features:</strong>
+            </p>
+            <ul>
+              <li>Chatroom where you can discuss and critique works</li>
+              <li>
+                Coming Soon: A Direct Messaging system so you can have pritvate
+                conversations with other users.
+              </li>
+              <li>Coming Soon: Friends List, Avatar,</li>
+            </ul>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Login />
+          </Col>
+          <Col>
+            <SignUp />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default HomePage;
-
-
-        
-  
