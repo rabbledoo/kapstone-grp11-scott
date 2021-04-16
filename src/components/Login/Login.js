76397159a99e45fcc7a1828a5c1a618770dd928d
Scott Tarnowsky <scott.tarnowsky@gmail.com>
2021-04-16 10:15:25 -0400

    clean up

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,20 +6,20 @@ function Login(props) {
     <Form id="login-body">
       <h3 id="login-top">Log-in</h3>
       <Form.Group controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="text" placeholder="Enter name" />
+        {/* <Form.Text className="text-muted">
           We'll never share your email with anyone else.
-        </Form.Text>
+        </Form.Text> */}
       </Form.Group>
 
       <Form.Group controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
         <Form.Control type="password" placeholder="Password" />
       </Form.Group>
-      <Form.Group controlId="formBasicCheckbox">
+      {/* <Form.Group controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
+      </Form.Group> */}
       <Button variant="primary" type="submit">
         Submit
       </Button>
