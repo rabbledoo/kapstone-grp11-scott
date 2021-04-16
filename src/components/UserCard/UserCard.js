cad6b65e6deb1b038f22f25b1e880ba313fdd4db
Scott Tarnowsky <scott.tarnowsky@gmail.com>
2021-04-16 09:35:22 -0400

    update usercard

--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -24,18 +24,30 @@ function UserCard() {
 
 
   return (
-    <Card style={{ width: '18rem' }}>
-    <Card.Body>
-      <Card.Title>Card Title</Card.Title>
-      <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-      <Card.Text>
-        Some quick example text to build on the card title and make up the bulk of
-        the card's content.
-      </Card.Text>
-      <Card.Link href="#">Card Link</Card.Link>
-      <Card.Link href="#">Another Link</Card.Link>
-    </Card.Body>
-  </Card>
+
+    <div id="userInfoCard">
+      <br />
+      <Card bg="secondary" text="white" style={{ width: "50rem" }}>
+        <Card.Header>Insert Username Here</Card.Header>
+        <Card.Body>
+          <Card.Title>Insert Displayname Here</Card.Title>
+          <Card.Text>Insert About the User Here</Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+
+  //   <Card style={{ width: '18rem' }}>
+  //   <Card.Body>
+  //     <Card.Title>Card Title</Card.Title>
+  //     <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+  //     <Card.Text>
+  //       Some quick example text to build on the card title and make up the bulk of
+  //       the card's content.
+  //     </Card.Text>
+  //     <Card.Link href="#">Card Link</Card.Link>
+  //     <Card.Link href="#">Another Link</Card.Link>
+  //   </Card.Body>
+  // </Card>
   )
 }
 export default UserCard;
